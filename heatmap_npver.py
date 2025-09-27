11a89bfcfd2d7a0fe03a849319bd8e4500b625e3
Eva Henderson <eva.henderson01@gmail.com>
2025-09-27 18:31:47 +0100

    Update heatmap_npver.py

--- heatmap_npver.py
+++ heatmap_npver.py
@@ -4,7 +4,15 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 gridsize = 100
-heatmap = np.array(gridsize,gridsize)
+
+mass1 = 0
+mass2 = 0
+
+temp1 = 0
+temp2 = 0
+
+heatmap = np.zeros((100,100))
+
 cmap = ListedColormap(['white', 'navy', 'mediumblue', 'yellowgreen', 'yellow','orange', 'red'])
 
 def get_user_float(message):
@@ -35,6 +43,44 @@ def get_info(info):
 
 
 
+def check_array(array = heatmap, v1=temp1, v2=temp2):
+    v1count = 0
+    v2count = 0
+
+    for x in range(100):
+        for y in range(100):
+            if array[x,y] == v1:
+                v1count +=1
+            if array[x,y] == v2:
+                v2count +=1
+    print('tiles temp 1: ' + str(v1count))
+    print('tiles temp 2: ' + str(v2count))
+    print('total should add to 10,000: ' + str(v1count+v2count))
+
+
+
+def set_start_info(info): #for testing
+    global mass1, mass2, temp1, temp2, max_t, min_t
+
+    if info == 'set':
+        mass1 = 100
+        temp1 = 300
+
+        mass2 = 60
+        temp2 = 250
+
+        print("mass1: " + str(mass1) + " temp1: " + str(temp1))
+        print("mass1: " + str(mass2) + " temp1: " + str(temp2))
+    
+    if info == 'min/max':
+        check_temp = [temp1, temp2]
+        max_t = max(check_temp)
+        min_t = min(check_temp)
+
+        set_info(max_t, min_t) #set intervals
+
+        return max_t, min_t
+
 def set_info(temp_max, temp_min):
     global temp_ranges
 
@@ -53,7 +99,7 @@ def set_colour(temp):
     for x in range(len(temp_ranges)):
         if temp in range(int(temp_ranges[x][0]), int(temp_ranges[x][1])+1):
             colour = x+2
-            print(str(temp) + " " + str((int(temp_ranges[x][0]), int(temp_ranges[x][1])+1)) + " " + str(colour))
+            #print(str(temp) + " " + str((int(temp_ranges[x][0]), int(temp_ranges[x][1])+1)) + " " + str(colour))
     
     return colour
 
@@ -69,6 +115,25 @@ def set_proportions():
 
     return tiles1, tiles2
 
+def set_heatmap(size = gridsize, tempgrid = heatmap):
+    global temp1, temp2
+
+    tiles1, tiles2 = set_proportions()
+    set1 = int(tiles1)
+
+    colour1 = set_colour(temp1)
+    colour2 = set_colour(temp2)
+
+    for x in range(0, set1):
+        for y in range(size):
+            tempgrid[x,y] = temp1
+    for x in range(set1, size):
+        for y in range(size):
+            tempgrid[x,y] = temp2
+
+    check_array()
+    print(heatmap)
+
 
 
 def temp_change():
@@ -80,29 +145,15 @@ def temp_change():
 
 
 
-def initialise_grid(size = gridsize):
+def initialise_grid(size = gridsize, tempgrid = heatmap):
     global temp1, temp2
 
-    grid = [[0 for x in range(size+1)] for x in range(size+1)] #geneate 100x100 pixel display
+    grid = [[0 for x in range(size)] for x in range(size)] #geneate 100x100 pixel display
 
-    tiles1, tiles2 = set_proportions()
-    set1 = int(tiles1)
-
-    colour1 = set_colour(temp1)
-    colour2 = set_colour(temp2)
-
-    print(str(temp1) + " " + str(colour1))
-    print(str(temp2) + " " + str(colour2))
-    
-    for x in range(0, set1):
+    for x in range(size):
         for y in range(size):
-            grid[y][x] = colour1
-            #print([x, y], Z[y][x])
-    
-    for a in range(set1, size):
-        for b in range(size):
-            grid[b][a] = colour2
-            #print([a, b], Z[b][a])
+            tile_colour = set_colour(tempgrid[x,y])
+            grid[y][x] = tile_colour
     
     for x in range(1, len(temp_ranges)+1):
         grid[0][x] = x
@@ -117,7 +168,7 @@ def grid_update(size = gridsize, tempgrid = heatmap):
     
     for x in range(100):
         for y in range(100):
-            tile_temp = tempgrid[x][y]
+            tile_temp = tempgrid[x,y]
             tile_colour = set_colour(tile_temp)
             grid[y][x] = tile_colour
     
@@ -131,8 +182,10 @@ def run(x):
 
 
 
-get_info(info = 'set')
-get_info(info = 'min/max')
+set_start_info(info = 'set')
+set_start_info(info = 'min/max')
+print(heatmap)
+set_heatmap()
 
 
 
